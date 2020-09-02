9a0dcd96a3f449c0b493bb1d71f24501bbc87105
Dan Kempton <kemptondan@gmail.com>
2020-09-02 18:57:42 -0400

    Creating axios instance - baseURL and headers

--- src/apis/unsplash.js
+++ src/apis/unsplash.js
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'https://api.unsplash.com/',
+  headers: {
+    Authorization: 'Client-ID FXEOlvCljnVeZlxf1rU79-CDQQ7kC5Ut5edI3uSOE18',
+  },
 });
