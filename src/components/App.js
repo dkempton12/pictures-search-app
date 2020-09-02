49b7655d8e525418443e627d1fe4823d92ea8e74
Dan Kempton <kemptondan@gmail.com>
2020-09-02 19:01:03 -0400

    Adding unsplash get request

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import unsplash from '../apis/unsplash';
 
 class App extends React.Component {
+  onFormSubmit = (event) => {
+    unsplash.get('/search/photos', {
+      params: { query: term },
+    });
+  };
   render() {
     return (
       <div className='ui container'>
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onFormSubmit} />
       </div>
     );
   }
