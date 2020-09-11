6c74043f6ee94ca5568591309ceca812f878fb6c
Dan Kempton <kemptondan@gmail.com>
2020-09-11 14:48:29 -0400

    Building ImageCard component for containing each image

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import unsplash from '../apis/unsplash';
+import ImageCard from './ImageCard';
 
 class App extends React.Component {
   state = {
@@ -17,6 +18,7 @@ class App extends React.Component {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onFormSubmit} />
+        <ImageCard />
       </div>
     );
   }
