6d1d12e6661c08afdc59c1ed2b13cebd7d591b1d
Dan Kempton <kemptondan@gmail.com>
2020-09-11 16:02:17 -0400

    Refactoring onSubmitForm method to an arrow function to bind this

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,17 @@ class App extends React.Component {
   state = {
     images: [],
   };
-  onFormSubmit = async (term) => {
+  onSearchSubmit = async (term) => {
     const response = await unsplash.get('/search/photos', {
       params: { query: term },
     });
     this.setState({ images: response.data.results });
-    console.log(this.state.results);
+    console.log(term);
   };
   render() {
     return (
       <div className='ui container'>
-        <SearchBar onFormSubmit={this.onFormSubmit} />
+        <SearchBar onSubmit={this.onSearchSubmit} />
         <ImageCard />
       </div>
     );
