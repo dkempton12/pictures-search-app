5e00189b289fc84bfa8688803a50ed4e80a90ea5
Dan Kempton <kemptondan@gmail.com>
2020-09-15 13:03:17 -0400

    Adding image-list class to images div

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const ImageList = (props) => {
   const images = props.images.map(({ urls, description, id }) => {
     return <img src={urls.regular} alt={description} key={id} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
